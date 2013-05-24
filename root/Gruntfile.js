5d3fbcc5737058ba9c5ff945e795c4d997976e17
Bernat Fortet <bernatfortet@gmail.com>
2013-05-24 01:05:16 -0700

    Structure refactor based on yeoman

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,95 +1,185 @@
 'use strict';
-var path = require('path');
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
-
-var folderMount = function folderMount(connect, point) {
-	return connect.static(path.resolve(point));
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
 };
-module.exports = function(grunt) {
 
-	grunt.initConfig({
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-		// LiveRelaod configuration
-		connect: {
-			livereload: {
-				options: {
-					port: 9001,
-					middleware: function(connect, options) {
-						return [lrSnippet, folderMount(connect, '.')]
-					}
-				}
-			}
-		},
+    // configurable paths
+    var basementConfig = {
+        app: 'app',
+        dist: 'dist'
+    };
 
-		// File Watching
-		regarde: {
-			jade: {
-				files: ['*.jade'],
-				tasks: ['jade','livereload']
-			},
-			less: {
-				files: ['*/**/*.less'],
-				tasks: ['less','livereload']
-			},
-			coffee: {
-				files: ['*/**/*.coffee'],
-				tasks: ['coffee','livereload']
-			}
-		},
+    grunt.initConfig({
+        basement: basementConfig,
+        watch: {
+            coffee: {
+                files: ['<%= basement.app %>/scripts/{,*/}*.coffee'],
+                tasks: ['coffee:dist']
+            },
+            jade: {
+                files: ["<%= basement.app %>/index.jade"],
+                tasks: ["jade"]
+            },
+            less: {
+                files: ['<%= basement.app %>/styles/{,*/}*.less'],
+                tasks: ['less:server']
+            },
+            livereload: {
+                files: [
+                    '<%= basement.app %>/*.html',
+                    '.tmp/index.html',
+                    '{.tmp,<%= basement.app %>}/styles/{,*/}*.css',
+                    '{.tmp,<%= basement.app %>}/scripts/{,*/}*.js',
+                    '<%= basement.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
+                ],
+                tasks: ['livereload']
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, 'app')
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        clean: {
+            dist: ['.tmp', '<%= basement.dist %>/*'],
+            server: '.tmp'
+        },
+        coffee: {
+            server: {
+                files: [{
+                    // rather than compiling multiple files here you should
+                    // require them into your main .coffee file
+                    expand: true,
+                    cwd: '<%= basement.app %>/scripts',
+                    src: '**/*.coffee',
+                    dest: '.tmp/scripts',
+                    ext: '.js'
+                }]
+            },
+            dist: {
+                files: [{
+                    // rather than compiling multiple files here you should
+                    // require them into your main .coffee file
+                    expand: true,
+                    cwd: '<%= basement.app %>/scripts',
+                    src: '**/*.coffee',
+                    dest: '<%= basement.dist %>/scripts',
+                    ext: '.js'
+                }]
+            }
+        },
+        jade: {
+            server: {
+                src: ['<%= basement.app %>/index.jade'],
+                dest: '.tmp/index.html',
+                options: {
+                    client: false,
+                    pretty: true
+                }
+            },
+            dist: {
+                src: ['<%= basement.app %>/index.jade'],
+                dest: '<%= basement.dist %>/index.html',
+                options: {
+                    client: false,
+                    pretty: false
+                }
+            }
+        },
+        less: {
+            options: {
+                paths: ['app/components'],
+                cssDir: '.tmp/styles',
+                imagesDir: '<%= basement.app %>/images',
+                javascriptsDir: '<%= basement.app %>/scripts',
+                fontsDir: '<%= basement.app %>/styles/fonts',
+                importPath: 'app/bower_components',
+                relativeAssets: true
+            },
+            server: {
+                options: {
+                    paths: ["<%= basement.app %>/styles"],
+                    yuicompress: true
+                },
+                files: {
+                    ".tmp/styles/main.css": "<%= basement.app %>/styles/main.less"
+                }
+            },
+            dist: {
+                options: {
+                    paths: ["<%= basement.app %>/styles"],
+                    yuicompress: true
+                },
+                files: {
+                    "<%= basement.dist %>/styles/main.css": "<%= basement.app %>/styles/main.less"
+                }
+            }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= basement.app %>',
+                    dest: '<%= basement.dist %>',
+                    src: [
+                        '*.{ico,txt}',
+                        '.htaccess',
+                        'images/{,*/}*.{webp,gif}'
+                    ]
+                }]
+            }
+        },
+    });
 
-		// Jade Configuration
-		jade: {
-			compile: {
-				files: {
-					'index.html': ['index.jade'],
-				}
-			}
-		},
+	grunt.renameTask('regarde', 'watch');
+    grunt.registerTask('server', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+        }
 
-		// Less Configuration
-		less: {
-			development: {
-				options: {
-					paths: ["assets/less"],
-					yuicompress: true
-				},
-				files: {
-					"assets/css/style.css": "assets/less/style.less"
-				}
-			}
-		},
+        grunt.task.run([
+            'clean:server',
+            'coffee:server',
+            'jade:server',
+            'less:server',
+            'livereload-start',
+            'connect:livereload',
+            'watch'
+        ]);
+    });
 
-		//Coffee Script Configuration
-		coffee: {
-			development:{
-				glob_to_multiple: {
-					expand: true,
-					cwd: 'assets/coffee',
-					src: ['**/*.coffee'],
-					dest: 'assets/scripts',
-					ext: '.js'
-				}
-			}
-		}
-	});
+    grunt.registerTask('build', [
+        'clean:dist',
+        'coffee:dist',
+        'less:dist',
+        'jade:dist',
+        'copy',
+    ]);
 
-
-
-
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-regarde');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-livereload');
-	grunt.loadNpmTasks('grunt-contrib-jade');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-
-	// Default task.
-	grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
-	
-	//grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
+    grunt.registerTask('default', ['livereload-start', 'connect', 'watch']);
 
 };
