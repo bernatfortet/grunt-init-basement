99ec64bd48d96bdfb81fbeffa908498578ab26ef
Bernat <socrattes@gmail.com>
2016-11-22 18:23:10 -1000

    Added imagmin

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -136,6 +136,16 @@ module.exports = function (grunt) {
         }
       }
     },
+    imagemin: {                          // Task
+      dynamic: {                         // Another target
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'dist/',                   // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'dist/'                  // Destination path prefix
+        }]
+      }
+    },
     copy: {
       dist: {
         files: [{
